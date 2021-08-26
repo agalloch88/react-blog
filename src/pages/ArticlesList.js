88c8ebb60f36ff372db4ebd929107e9af446871c
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2021-08-26 10:52:35 -0400

    add article routing and links, plus text preview

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import articleContent from './article-content';
+import { Link } from 'react-router-dom';
 
 function ArticlesList() {
     return (
         <>
             <h1>List of Articles</h1>
+            {articleContent.map((article, key) => (
+                <Link className="article-list-item" key={key} to={`/article/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0, 150)}...</p>
+                </Link>
+            ))}
         </>
     )
 }
