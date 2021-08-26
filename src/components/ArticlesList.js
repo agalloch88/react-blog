2fa5edb14b3107b7a0130a49adb177a34bfa24ba
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2021-08-26 12:01:08 -0400

    refactor ArticleList to whole Page component, extract ArticleList code to separate component. implement related article list

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import articleContent from './article-content';
 import { Link } from 'react-router-dom';
 
-function ArticlesList() {
+function ArticlesList({ articles }) {
     return (
         <>
-            <h1>List of Articles</h1>
-            {articleContent.map((article, key) => (
+            {articles.map((article, key) => (
                 <Link className="article-list-item" key={key} to={`/article/${article.name}`}>
                     <h3>{article.title}</h3>
                     <p>{article.content[0].substring(0, 150)}...</p>
